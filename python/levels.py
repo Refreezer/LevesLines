6b009ac1f70b33026b79a047f9e6b1ece1b7060c
refreezer <Shneider.Yarik@gmail.com>
2020-07-25 13:51:20 +0300

    changed lib to pylab

--- python/levels.py
+++ python/levels.py
@@ -1,25 +1,24 @@
 from numpy import *
-import matplotlib.pyplot as plt
 import sys
 import random
+import pylab
 
-func = sys.argv[1]
+func =sys.argv[1]
 
 
-
-print(func)
 
 x, y = mgrid[-3*pi:3*pi:100j, -3*pi:3*pi:300j]
 
-
 z = eval(func)
 
-fig, ax = plt.subplots()
+if __name__ == '__main__':
+    cs = pylab.contour(x, y, z, 20)
+    # pylab.clabel(cs)
 
-ax.contour(z, levels = 50)
 
-fig.set_figwidth(9)
-fig.set_figheight(9)
+
 name = str("images\\" + str(random.randint(1,100)) + ".png")
-plt.savefig(name)
+pylab.savefig(name)
 print(name)
+# следующая строка вызывает окно pylab
+pylab.show()
